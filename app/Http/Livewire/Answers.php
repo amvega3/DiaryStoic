6ef85b9eb7050e9f0530c33840dd4771df7042db
Apvasquez5 <apvasquez5@espe.edu.ec>
2022-08-28 22:59:40 -0500

    datev3

--- app/Http/Livewire/Answers.php
+++ app/Http/Livewire/Answers.php
@@ -18,24 +18,33 @@ class Answers extends Component
     {
         $this->message = "You clicked on button";
     }
-    
+
     public $answers1, $answers2, $answers3, $answers4, $answers5;
     public function render()
     {
         $answers = Answer::all();
-        // dd($questions);
-        
-        return view('livewire.answers',compact('answers'));
+
+        if ($this->showingModal_answers) {
+            $data = $this->answer_All;
+
+        }
+        else
+        {
+
+            $data = null;
+        }
+
+        return view('livewire.answers',compact('answers','data'));
 
     }
     public function showModal(){
-        $this->showingModal = true;   
-        $this->showingModal = !$this->showingModal;           
+        $this->showingModal = true;
+        $this->showingModal = !$this->showingModal;
     }
     public function showModal_answer(){
-        $this->showingModal_answers = true;   
-        $this->showingModal_answers = !$this->showingModal_answers;      
-                     
+        $this->showingModal_answers = true;
+        $this->showingModal_answers = !$this->showingModal_answers;
+
     }
     public function store()
     {
@@ -44,39 +53,39 @@ class Answers extends Component
             'user_id'=> auth()->user()->id,
             'answer' => $this->answers1,
             'question_id' => 1,
-            'pathaudio'=> " "          
-  
+            'pathaudio'=> " "
+
         ]);
-        
+
         Answer::create([
             'user_id'=> auth()->user()->id,
             'answer' => $this->answers2,
             'question_id' => 2,
             'pathaudio'=> " "
-  
+
         ]);
-        
+
         Answer::create([
             'user_id'=> auth()->user()->id,
             'answer' => $this->answers3,
             'question_id' => 3,
             'pathaudio'=> " "
-  
+
         ]);
         Answer::create([
             'user_id'=> auth()->user()->id,
             'answer' => $this->answers4,
             'question_id' => 4,
             'pathaudio'=> " "
-  
+
         ]);
-    
+
         Answer::create([
             'user_id'=> auth()->user()->id,
             'answer' => $this->answers5,
             'question_id' => 5,
             'pathaudio'=> " "
-  
+
         ]);
 
         $this->default();
@@ -88,10 +97,9 @@ class Answers extends Component
     }
 
     public function showAllAnswers(Answer $date){
+
+        $this->answer_All = Answer::where('created_at',$date->created_at)->get();
         $this->showingModal_answers=true;
-        $answer_All = Answer::find($date->id)->get();
-        #dd($answer_All);
-        return view('livewire.answers',compact('answer_All'));
     }
 
 }
--- resources/views/livewire/answers.blade.php
+++ resources/views/livewire/answers.blade.php
@@ -2,54 +2,55 @@
     <div class="flex space-x-2 justify-center pb-5">
         <!-- <button  class="px-2 py-2 mx-auto inline-block rounded-full bg-[#FFC600] text-white leading-normal uppercase shadow-md ease-in-out w-9 h-9 " wire:click="$set('showingModal',true)"> <h3>+</h3></button> -->
         <!-- <img src="{{ asset('img/plus (1).png') }}" alt="" width="100" height="200" class="px-2 py-2 mx-auto inline-block rounded-full  text-white leading-normal uppercase shadow-md ease-in-out w-9 h-9 " wire:click="$set('showingModal',true)" /> -->
-        <button
-            class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center" wire:click="$set('showingModal',true)">
+        <button class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center" wire:click="$set('showingModal',true)">
             <img src="{{ asset('img/plus (1).png') }}" alt="" width="300" height="200"
-                class="px-2 py-2 mx-auto inline-block rounded-full  text-white leading-normal uppercase shadow-md ease-in-out w-9 h-9 "
-                wire:click="$set('showingModal',true)" />
+                class="px-2 py-2 mx-auto inline-block rounded-full  text-white leading-normal uppercase shadow-md ease-in-out w-9 h-9 " wire:click="$set('showingModal',true)" />
 
             <span class="text-[#FFC600]">Agregar nota</span>
         </button>
     </div>
 
-    {{$answer_All}}
     <div class="divide-y  rounded-xl divide-slate-100 bg-[#FFC600] mr-40 ml-40 text-black ">
 
         <x-list>
             @foreach ($answers as $answer)
-           <div wire:click="showAllAnswers({{$answer}})">
-
-                @if ($answer->question_id == 5)
-                    <x-list-item :answer="$answer" />
+                @if ($answer->question_id == 1)
+                    <div wire:click="showAllAnswers({{ $answer }})">
+                        <x-list-item :answer="$answer" />
+                    </div>
                 @endif
-        </div>
-
             @endforeach
         </x-list>
 
     </div>
 
+
     @include('answers.form');
     <!-- @include('answers.allAnswers') -->
 
-    @if($showingModal_answers == true)
-    <x-jet-dialog-modal wire:model="showingModal_answers" >
+    @if ($showingModal_answers == true)
+        <x-jet-dialog-modal wire:model="showingModal_answers">
 
-    <x-slot name="title">
-        Notal del dia
-        {{$answer_All}}
-    </x-slot>
+            <x-slot name="title">
+                Notal del dia -
 
-    <x-slot name="content">
+            </x-slot>
 
-    <h1 class="text-white">{{$answer_All}}</h1>
+            <x-slot name="content">
+
+                @foreach ($data as $answer)
+                    {{ $answer->id }}
+                @endforeach
 
 
-    </x-slot>
 
-    <x-slot name="footer">
-    </x-slot>
 
-    </x-jet-dialog-modal>
-@endif
+
+            </x-slot>
+
+            <x-slot name="footer">
+            </x-slot>
+
+        </x-jet-dialog-modal>
+    @endif
 </div>
